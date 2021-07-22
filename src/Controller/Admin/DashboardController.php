68687929810a58d5eb324e7807be5be8ebd3f1f7
Hafedh Amri <amri_hafedh@yahoo.com>
2021-07-22 13:16:41 +0200

    rentCarProject

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Entity\Cars;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Security\Core\User\UserInterface;
 
 
 
@@ -32,6 +35,18 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+
+        yield MenuItem::section('Utilisateurs');
+        yield MenuItem::linkToCrud('Liste', 'fas fa-users', User::class);
+        yield MenuItem::linkToCrud('Ajout', 'fas fa-user-plus', User::class)->setAction('new');
+
+        yield MenuItem::section('Voitures');
+        yield MenuItem::linkToCrud('Liste', 'fas fa-car', Cars::class);
+        yield MenuItem::linkToCrud('Ajout', 'fas fa-car', Cars::class)->setAction('new');
+
+        // yield MenuItem::linkToCrud('Conferences', 'fa fa-map-marker', Conference::class);
+        //yield MenuItem::linkToCrud('Comments', 'fa fa-comments', Comment::class);
+        // yield MenuItem::linkToCrud('The Label', 'icon class', EntityClass::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
